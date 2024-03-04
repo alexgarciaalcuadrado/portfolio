11460b96d6efcdff3bf6dcfd67a88447c773d2f8
alexgarciaalcuadrado <alexgarciaalcuadrado@gmail.com>
2024-03-04 15:45:38 -0300

    add skills

--- components/Skils.tsx
+++ components/Skils.tsx
@@ -45,6 +45,9 @@ const Skils = () => {
             VSCode, Android Studio
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max mr-4">
+            Git
+          </div>
+          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max mr-4">
             Jest testing
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max mr-4">
@@ -70,6 +73,9 @@ const Skils = () => {
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max mr-4">
             Supabase and Firebase database management
+          </div>
+          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max mr-4">
+            Web Accessibility and SEO
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max mr-4">
             Detail-oriented
